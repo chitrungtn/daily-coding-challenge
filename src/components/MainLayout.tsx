3928b800bbb61dadd2a06f84e7c0bdb8638f1629
tuan vu <tuan.vu@base.vn>
2024-12-05 21:53:46 +0700

    add layout

---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,14 @@
+
+const MainLayout = (): JSX.Element => {
+    return (
+        <div>
+
+            <main >
+                <div>helu</div>
+            </main>
+
+        </div>
+    )
+}
+
+export default MainLayout;
